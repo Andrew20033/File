df00f9404abd5f2d41e302ef1948d6e932610ab1
Stuk_uc23@student.itstep.org <Stuk_uc23@student.itstep.org>
2022-10-28 18:39:30 +0300

    Добавьте файлы проекта.

---
+++ File/Program.cs
@@ -0,0 +1,28 @@
+﻿using FastSearchLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace File
+{
+    class Program
+    {
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string[] files = Directory.GetFiles(path: @"C:\Windows\", searchPattern: "*.jpg", searchOption: SearchOption.AllDirectories);
+                NewMethod(files: files);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message + "" + ex.Source);
+
+            }
+
+        }
+    }
+}
